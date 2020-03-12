e8be3b1de0ed1b5d7adeeb3ee6bdd7b75033c4b5
G3yost <G3yost@Gmail.com>
2020-03-11 22:49:30 -0700

    Updated K9, and also finished hoework 3.

--- Project3_Database/main.js
+++ Project3_Database/main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 
+    var socket = io.connect("http://24.16.255.56:8888");
     var canvas = document.getElementById('gameWorld');
     var ctx = canvas.getContext('2d');
 
@@ -34,6 +35,43 @@ window.onload = function() {
             squares[i][j].addNeighbors(squares[topp][left], squares[topp][i], squares[topp][right], squares[left][j], squares[right][j], squares[bottom][left], squares[bottom][i], squares[bottom][right]);
         }
     }
+
+    socket.on("load", function (data) {
+        console.log(data);
+        console.log(data.data);
+
+        for(i = 0; i < gameEngine.xDim; i++) {
+            for(j = 0; j < gameEngine.yDim; j++) {
+
+                squares[i][j].isAlive = data.data[i][j];
+            }
+        }
+    });
+
+    var text = document.getElementById("text");
+    var saveButton = document.getElementById("save");
+    var loadButton = document.getElementById("load");
+
+    saveButton.onclick = function () {
+
+        console.log("saving");
+        var saveArr = [];
+
+        for(i = 0; i < gameEngine.xDim; i++) {
+
+            saveArr[i] = [];
+
+            for(j = 0; j < gameEngine.yDim; j++) { saveArr[i][j] = squares[i][j].isAlive; }
+        }
+
+        socket.emit("save", { studentname: "Gary Yost III", statename: "Running", data: saveArr });
+        console.log("saved");
+    };
+
+    loadButton.onclick = function () {
+        console.log("load");
+        socket.emit("load", { studentname: "Gary Yost III", statename: "Running" });
+    };
 
     gameEngine.init(ctx);
     gameEngine.start();
